30122b235aa9d0f8941121e22f9e048f3a0e93b8
darts <darts@tcd.ie>
2019-10-23 19:49:39 +0100

    Added some comments

--- Ex02.hs
+++ Ex02.hs
@@ -43,34 +43,34 @@ v42 = Val 42 ; j42 = Just v42
 
 
 eval :: EDict -> Expr -> Maybe Double
-eval d (Var e) = find d e
-eval _ (Val e) = Just e
-eval _ (Dvd e (Val 0)) = Nothing 
+eval d (Var e) = find d e --find the Var e in the dict d
+eval _ (Val e) = Just e --no need to find Val in dict, just return it
+eval _ (Dvd e (Val 0)) = Nothing --prevent divide by zero 
 
 eval d (Add a b) 
-  = case (eval d a, eval d b) of
-    (Just x, Just y) -> Just (x+y)
+  = case (eval d a, eval d b) of  --evaluate both expressions for pattern match
+    (Just x, Just y) -> Just (x+y) --if both expressions are valid, return the sum
     _                -> Nothing 
 
 eval d (Mul a b) 
-  = case (eval d a, eval d b) of
-    (Just x, Just y) -> Just (x*y)
+  = case (eval d a, eval d b) of  --evaluate both expressions for pattern match
+    (Just x, Just y) -> Just (x*y) --if both expressions are valid, return the product
     _                -> Nothing 
 
 eval d (Sub a b) 
-  = case (eval d a, eval d b) of
-    (Just x, Just y) -> Just (x-y)
+  = case (eval d a, eval d b) of  --evaluate both expressions for pattern match
+    (Just x, Just y) -> Just (x-y) --if both expressions are valid, return the difference
     _                -> Nothing 
 
 eval d (Dvd a b) 
-  = case (eval d a, eval d b) of
-    (Just x, Just y) -> Just (x/y)
+  = case (eval d a, eval d b) of  --evaluate both expressions for pattern match
+    (Just x, Just y) -> Just (x/y) --if both expressions are valid, return the div
     _                -> Nothing 
 
 eval d (Def n a b)
-  = let x = eval d a
-  in case (x) of 
-    Just x -> eval (define d n x) b
+  = let x = eval d a --evaluate expr a so we can use the result for b
+  in case (x) of --check what's up with the result of expr a
+    Just x -> eval (define d n x) b --add the result of expr a to the dictionary and then use that new dict for expr b
     _      -> Nothing
 
 eval d e = Just 1e-99
@@ -103,31 +103,31 @@ There are many, many laws of algebra that apply to our expressions, e.g.,
 
 law1 :: Expr -> Maybe Expr
 law1 thing 
-    = case thing of
-      (Add x y) -> Just (Add y x)
+    = case thing of --if it's real
+      (Add x y) -> Just (Add y x) --just swap
       _         -> Nothing
 
 law2 :: Expr -> Maybe Expr
 law2 thing
-    = case thing of
-      (Add x (Add y z)) -> Just (Add (Add x y) z)
+    = case thing of --if it's real
+      (Add x (Add y z)) -> Just (Add (Add x y) z) --just swap
       _                 -> Nothing
 
 law3 :: Expr -> Maybe Expr
 law3 thing
-    = case thing of
-      (Sub x (Add y z)) -> Just (Sub (Sub x y) z)
+    = case thing of --if it's real
+      (Sub x (Add y z)) -> Just (Sub (Sub x y) z) --just swap
       _                 -> Nothing
 
 
 law4 :: Expr -> Maybe Expr
 law4 thing  
-    =case (thing) of
-      (Mul (Add a b) (Sub c d)) -> law4hlp (a==c) (b==d) (Sub (Mul a c) (Mul b d))
+    =case (thing) of --if it's real
+      (Mul (Add a b) (Sub c d)) -> law4hlp (a==c) (b==d) (Sub (Mul a c) (Mul b d)) --call the helper function to eval the bools and return the right thing
       _                                     -> Nothing
       
 law4hlp :: Bool -> Bool -> Expr -> Maybe Expr
 law4hlp a b c
-        =case (a, b) of
-          (True, True) -> Just c
+        =case (a, b) of 
+          (True, True) -> Just c --if a==c and b==d, return the new function
           (_, _)       -> Nothing
