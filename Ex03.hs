abfce0d638c045f9b4047773eeadcfcdf0db4292
darts <darts@tcd.ie>
2019-11-11 19:14:06 +0000

    21/21

--- Ex03.hs
+++ Ex03.hs
@@ -46,5 +46,4 @@ module Ex03 where
   -- convert a binary search tree into an (ordered) association list
   bst2assoc :: Ord c =>  BT c e -> Assoc c e
   bst2assoc Leaf = []
-  bst2assoc _ = error "bst2assoc not yet implemented"
-  
+  bst2assoc (Branch left a b right) = concat[bst2assoc left, [(a,b)], bst2assoc right]
